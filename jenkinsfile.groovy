c72948f43d164986c36083d5aee093e6c19f3155
AGGARWAL Nimit <nimit.aggarwal@soprasteria.com>
2022-01-06 23:27:24 +0530

    a

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -16,7 +16,7 @@ import groovy.io.*
         steps {
           script {
             def outputp = powershell(returnStatus: true, script: 'gci "C:\\Users\\naggarwal\\Documents\\GitHub\\securego_internal\\Results" | sort -Property LastWriteTime -Descending | select -First 1').toString()
-			outputp = outputp.substring(outputp.findIndexOf('Result_'))
+			outputp = outputp.substring(outputp.indexOf('Result_'))
 			println "${outputp}"
             echo "Generating result.........."
           }
