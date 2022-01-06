14a44d50b042d0b30697b9b8abb65c2a1af3413e
AGGARWAL Nimit <nimit.aggarwal@soprasteria.com>
2022-01-07 02:35:17 +0530

    s

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -26,7 +26,8 @@ def resultFileName = "VRBankingResultFile.html"
 			currentResultFolder =  new File(currentResultFolder, resultFileName).toString()
 			println("${currentResultFolder}")
             echo "Generating result.........."
-			archiveArtifacts artifacts: "${outputp}"/*.html
+			println(new File(".").getAbsolutePath().toString())
+			archiveArtifacts artifacts: "${outputp}/*.html"
 			bat "copy currentResultFolder new File(".").getAbsolutePath().toString()"
 
           }
