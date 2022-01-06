e54c669920026a786c4b28ceaac2e62945fd73f3
AGGARWAL Nimit <nimit.aggarwal@soprasteria.com>
2022-01-07 00:22:31 +0530

    s

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -16,8 +16,7 @@ def resultfolder = "C:\\Users\\naggarwal\\Documents\\GitHub\\securego_internal\\
       stage('Generating result') {
         steps {
           script {
-            println(resultfolder)
-			def outputp = powershell(returnStdout: true, script: 'gci ${resultfolder} | sort -Property LastWriteTime -Descending | select -First 1').toString()
+			def outputp = powershell(returnStdout: true, script: 'gci "C:\\Users\\naggarwal\\Documents\\GitHub\\securego_internal\\Results" | sort -Property LastWriteTime -Descending | select -First 1').toString()
 			println("${outputp}")
 			echo "Printing powershell output.........."
 			outputp = outputp.substring(outputp.indexOf("Result_"))
